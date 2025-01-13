ad089592105ecc4e346b91eccdf1ec276b760232
Anton Alekseev <anton.m.alexeyev@gmail.com>
2025-01-13 15:39:40 +0300

    Metainfor extraction script added; minor fixes

--- meta_extractor.py
+++ meta_extractor.py
@@ -3,8 +3,8 @@ import csv
 input_file = "ky_ktmu-ud-test.conllu"
 output_file = "ky_ktmu-ud-test.conllu.tsv"
 
-input_file = "ky_ktmu-ud-train.conllu"
-output_file = "ky_ktmu-ud-train.conllu.tsv"
+# input_file = "ky_ktmu-ud-train.conllu"
+# output_file = "ky_ktmu-ud-train.conllu.tsv"
 
 # Initialize variables
 sentences = []
@@ -28,6 +28,6 @@ with open(output_file, "w", encoding="utf-8", newline="") as tsvfile:
     tsv_writer = csv.writer(tsvfile, delimiter="\t")
     tsv_writer.writerow(["sent_id", "text", "genre", "url"])
     for sent_id, text in sentences:
-        tsv_writer.writerow([sent_id, text, "", ""])
+        tsv_writer.writerow([sent_id, text.strip(), "", ""])
 
 print(f"TSV file generated: {output_file}")
